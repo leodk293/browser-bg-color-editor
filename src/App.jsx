a307a2d455ec64c0e1e7dd1ad7ea25a08fc47023
Aboubacar Traore <aboubatrao04@gmail.com>
2025-08-16 09:58:04 +0100

    browser-bg-color-editor

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("");
+
+  const onClick = async () => {
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      args: [color],
+      func: (color) => {
+        //alert("hello");
+        document.body.style.backgroundColor = color;
+      },
+    });
+  };
+
+  const cancel = async () => {
+    let [tab] = await chrome.tabs.query({ active: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+        //alert("hello");
+        document.body.style.backgroundColor = "";
+      },
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <p className=" text-center font-medium text-white text-3xl">Test</p>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={onClick}>Click</button> 
+        <input
+          onChange={(event) => setColor(event.target.value)}
+          type="color"
+        />
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+        <button onClick={cancel}>Cancel</button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
